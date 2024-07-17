dff86e7f44e4f446844a8dcd9a7c078f429edecd
MarkAyl5 <148015142+MarkAyl5@users.noreply.github.com>
2024-07-17 19:25:09 -0400

    Update server.js

--- app/server.js
+++ app/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql');
 const app = express();
 const cors = require('cors');
-const port = process.env.MYSQL_ADDON_PORT || 3000;
+const port = process.env.PORT || 3000;
 require('dotenv').config();
 
 app.use(cors());
